271b6e0f52947729e6e425bd8b314e172aa78048
Vasanth Chandrasekar <35191203+Vasanth14@users.noreply.github.com>
2023-09-07 18:32:06 +0530

    Added routes

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,18 +1,24 @@
 import {Card} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Rating from './Rating';
 
 
 const Product = ({product}) => {
     return(
         <Card className='my-3 p-3 rounded'>
-            <a href={`/products/${product._id}`}>
+            <Link  to={`/products/${product._id}`}>
                 <Card.Img src={product.image} variant='top' />
-            </a>
+            </Link>
             <Card.Body>
-                <a href={`/product/${product._id}`}>
-                    <Card.Title as="div">
+                <Link to={`/product/${product._id}`}>
+                    <Card.Title as="div" className='product-title'>
                         <strong>{product.name}</strong>
                     </Card.Title>
-                </a>
+                </Link>
+                <Card.Text as='div'>
+                    <Rating value={product.rating} text={`${product.numReviews}reviews`} />
+                </Card.Text>
+
                 <Card.Text as='h3'>
                 ₹{product.price}
                 </Card.Text>
